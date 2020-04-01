e84e63d15798812df501f1a1b6735a1a46357bdf
ramaranjandey <36834875+ramaranjandey@users.noreply.github.com>
2020-04-01 14:00:24 +0530

    updated clickable card.

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -4,10 +4,10 @@ import React from 'react';
 
 const User = (props) => {
     return (
-        <li className="w3-bar" style={{ backgroundColor: "#fff" }} key={props.id}>
+        <li className="w3-bar" style={{ backgroundColor: "#fff" }} key={props.id} onClick={() => props.activePeriod(props.id)}>
             <img src="/assets/avatar.jpg" alt="avatar" className="w3-bar-item w3-circle w3-hide-small" style={{ width: "85px" }}></img>
             <div className="w3-bar-item">
-                <div className="w3-large" style={{ color: "blue" }} onClick={() => props.activePeriod(props.id)}>
+                <div className="w3-large" style={{ color: "blue" }}>
                     {props.name}
                 </div>
                 <div className="w3-medium">{props.place}</div>
